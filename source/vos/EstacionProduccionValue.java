ce0e77abaa1c667708bf6364279542d7c86c2019
cf.agudelo12 <cf.agudelo12>
2015-04-26 13:02:51 -0500

    Se adelanta un poco la iteracion

--- source/vos/EstacionProduccionValue.java
+++ source/vos/EstacionProduccionValue.java
@@ -2,81 +2,93 @@ package vos;
 
 public class EstacionProduccionValue 
 {
-		//----------------------------------------------------
-		// Constantes
-		//----------------------------------------------------
-		
-		public final static String activa = "Activa";
-		
-		public final static String inactiva = "Inactiva";
-		
-		public final static String cIdEstacionProduccion="idEstacionProduccion";
-		
-		public final static String cCapacidadProduccion="capacidadProduccion";
-		
-		public final static String cNombre="nombre";
-		
-		public final static String cEstado="estado";
-		
-		public final static String cIdEmpresa="idEmpresa";
-		
-		//----------------------------------------------------
-		// Atributos
-		//----------------------------------------------------
-		
-		private int idEstacionProduccion;
-		
-		private int capacidadProduccion;
-		
-		private String nombre;
-		
-		private String estado;
-		
-		private int idEmpresa;
-		
-		//----------------------------------------------------
-		// Métodos
-		//----------------------------------------------------
-		
-		public EstacionProduccionValue(){}
+	//----------------------------------------------------
+	// Constantes
+	//----------------------------------------------------
 
-		public int getIdEstacionProduccion() {
-			return idEstacionProduccion;
-		}
+	public final static String activa = "Activa";
 
-		public void setIdEstacionProduccion(int idEstacionProduccion) {
-			this.idEstacionProduccion = idEstacionProduccion;
-		}
+	public final static String inactiva = "Inactiva";
 
-		public int getCapacidadProduccion() {
-			return capacidadProduccion;
-		}
+	public final static String cIdEstacionProduccion="idEstacionProduccion";
 
-		public void setCapacidadProduccion(int capacidadProduccion) {
-			this.capacidadProduccion = capacidadProduccion;
-		}
+	public final static String cCapacidadProduccion="capacidadProduccion";
 
-		public String getNombre() {
-			return nombre;
-		}
+	public final static String cNombre="nombre";
 
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-		
-		public String getEstado(){
-			return estado;
-		}
-		
-		public void setEstado(String estado){
-			this.estado = estado;
-		}
-		
-		public int getIdEmpresa(){
-			return idEmpresa;
-		}
-		
-		public void setIdEmpresa(int idEmpresa){
-			this.idEmpresa = idEmpresa;
-		}
+	public final static String cEstado="estado";
+
+	public final static String cIdEmpresa="idEmpresa";
+	
+	public final static String cTiempoEjecucion="tiempoEjecucion";
+
+	//----------------------------------------------------
+	// Atributos
+	//----------------------------------------------------
+
+	private int idEstacionProduccion;
+
+	private int capacidadProduccion;
+
+	private String nombre;
+
+	private String estado;
+
+	private int idEmpresa;
+
+	private int tiempoEjecucion;
+
+	//----------------------------------------------------
+	// Métodos
+	//----------------------------------------------------
+
+	public EstacionProduccionValue(){}
+
+	public int getIdEstacionProduccion() {
+		return idEstacionProduccion;
+	}
+
+	public void setIdEstacionProduccion(int idEstacionProduccion) {
+		this.idEstacionProduccion = idEstacionProduccion;
+	}
+
+	public int getCapacidadProduccion() {
+		return capacidadProduccion;
+	}
+
+	public void setCapacidadProduccion(int capacidadProduccion) {
+		this.capacidadProduccion = capacidadProduccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getIdEmpresa(){
+		return idEmpresa;
+	}
+
+	public void setIdEmpresa(int idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+
+	public int getTiempoEjecucion(){
+		return tiempoEjecucion;
+	}
+
+	public void setTiempoEjecucion(int tiempoEjecucion) {
+		this.tiempoEjecucion = tiempoEjecucion;
+	}
 }
