d2856a69f2bdb4630ea872950ee221548983e70a
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2015-05-18 12:05:47 -0500

    new jars

--- Nuevo Proyecto/ProdAndes5_cf_agudelo12_sy_velasquez10/src/dao/ConsultaDAO.java
+++ Nuevo Proyecto/ProdAndes5_cf_agudelo12_sy_velasquez10/src/dao/ConsultaDAO.java
@@ -209,14 +209,14 @@ public class ConsultaDAO extends oracle.jdbc.driver.OracleDriver
 	{
 		try 
 		{
-//			Hashtable<String,String> env=new Hashtable<String,String>();
-//			env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-//			env.put(Context.PROVIDER_URL, "jnp://localhost:1099");
-//			env.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
-//			contexto=  new InitialContext(env);
-//			ds2=(DataSource)contexto.lookup("java:XAChie2");
-//			cf=(ConnectionFactory)contexto.lookup("java:JmsXA");
-//			cola= (Queue) contexto.lookup("queue/WebApp2");
+			Hashtable<String,String> env=new Hashtable<String,String>();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+			env.put(Context.PROVIDER_URL, "jnp://localhost:1099");
+			env.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+			contexto=  new InitialContext(env);
+			ds2=(DataSource)contexto.lookup("java:XAChie2");
+			cf=(ConnectionFactory)contexto.lookup("java:JmsXA");
+			cola= (Queue) contexto.lookup("queue/WebApp2");
 		}
 		catch (Exception e) 
 		{
